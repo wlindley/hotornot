b366ef9ac6a08b9329505aa84fbc66247ec68115
Walker Lindley <walker@walkerlindley.com>
2012-09-22 14:42:57 -0700

    Starting to pull FB friend list in JS.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(function() {
-	$("div#hello").show()
-})
+	console.log("on ready");
+	$("div#hello").show();
+
+	function log(text) {
+		if (undefined !== console) {
+			console.log(text);
+		}
+	}
+	
+	function friendLoadSuccess(data) {
+		log("ajax friend load success");
+		friendHtml = "";
+		for (friend in data.data) {
+			friendHtml += friend.name + "<br>";
+		}
+		$("div#friendlist").html(friendHtml);
+	}
+
+	function friendLoadFailure(data) {
+		log("ajax friend load failed");
+	}
+
+	//$.ajax("http://graph.facebook.com/me/friends", {"success" : friendLoadSuccess, "error" : friendLoadFailure});
+	log("ajax call for friends fired");
+});
